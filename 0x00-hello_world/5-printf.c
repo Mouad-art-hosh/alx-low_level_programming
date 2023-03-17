fa086b384cc08641126c955f36e2d3a51e1a48ad
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-16 19:03:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - xalili a baba 
+ *
+ * Return: 0 will back
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}	
