3c46c3b58f41b333f2d20aa58288e125c570665d
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-16 18:42:53 -0700

    ts7i7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
