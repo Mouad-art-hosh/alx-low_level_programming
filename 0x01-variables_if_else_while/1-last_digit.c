a47580e491b4d00caa9283a2e27f142bf040fb56
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-19 15:28:53 -0700

    cc

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+#include <stdio.h
 
 /**
  * maine - Prints a text according number
