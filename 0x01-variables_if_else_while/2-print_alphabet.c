008b5fa47fa36d0734d147189f99b25ccef57645
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-20 15:11:00 -0700

    @hi@

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Reaturn: always 0
+ */
+int main(void)
+{
+	char (i);
+
+
+	for (i = 'a' ; i <= 'z' ; i++);
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
