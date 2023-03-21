23230786b8a029ce123ee8e702f71633dd4fd69d
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-20 19:59:46 -0700

    hk

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,8 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; i++)
-		putchar('\n');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
 	return (0);
 }
