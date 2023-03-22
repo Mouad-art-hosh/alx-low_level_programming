1cc13d19989b242e4aaabed9374bdef8730f4afa
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-21 20:13:45 -0700

    dx

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,11 @@
  * Return: always 0
  */
 
-void print_alphabet(void);
-
+void print_alphabet(void)
 {
-	char i;
+		char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+		for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
 		_putchar('\n');
 }
