4ee0f46f4a1587263193136f5a4a060f82a12116
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-21 19:35:51 -0700

    2-, 10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,16 @@
 
 /**
  * main - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: always 0
  */
 
-void print_alphabet(void)
-{
-	char letter;
+void print_alphabet(void);
 
-	for (letter = 'a'; letter <= 'z' ; letter++);
-_putchar(letter);
-_putchar('\n');
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+		_putchar('\n');
 }
