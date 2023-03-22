839f40167d121eea170800f8f140fd2b018aedd1
Mouad-atr-hosh <mouadyelouachi@icloud.com>
2023-03-21 20:05:22 -0700

    ok

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - times the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10 - function that prints 10x alphabets
  *
  *Return: always 0
  */
