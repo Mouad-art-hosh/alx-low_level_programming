8ba2b497284199aad565533d9f139c9101c8672c
mouad-art-hosh <mouadyelouachi@icloud.com>
2023-04-28 18:51:27 -0700

    qw

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,12 +4,11 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t b = 0;
 
 	while (h)
 	{
@@ -18,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		b++;
 	}
-	return (s);
+	return (b);
 }
