f108930ef61be0a98786eba0748a076c1c09c40e
mouad-art-hosh <mouadyelouachi@icloud.com>
2023-05-02 13:09:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t b = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		b++;
+		num++;
 		h = h->next;
 	}
-	return (b);
+	return (num);
 }
